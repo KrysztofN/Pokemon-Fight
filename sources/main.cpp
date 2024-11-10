a1d684f9fb249b55457cabb7ccbcc89bba7a984e
Krzysztof <kris1toja@gmail.com>
2024-11-10 23:35:43 +0100

    added function displayLoadingBar

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,6 +3,9 @@
 #include "../headers/trainer.h"
 #include <fstream>
 #include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <iomanip>
 
 void clearScreen(){
     #ifdef _WIN32
@@ -10,6 +13,17 @@ void clearScreen(){
     #else
         system("clear");
     #endif
+}
+
+void displayLoadingBar(const std::string& message){
+    std::cout << message;
+    for(int i = 0; i<20; i++){
+        std::cout << " ";
+        std::cout.flush();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    std::cout << "Done !" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
 }
 
 int main(){
